8ef66233a6f04fd1de1f0264b6527f815aed4572
MӨӨП KПIGΉƬ <91146761+MxxnKnight@users.noreply.github.com>
2023-04-01 21:08:38 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -30,7 +30,7 @@ Thanks for thinking about us.\nIt seems that you really love <a href=https://t.m
 \nSorry sweetie.. You must have to be the Authentic User to complete this operation...\n\n👮‍♀ REPORT ISSUE HERE: <a href=https://t.me/LazyDeveloperSupport>LazyDeveloper Support</a>\n\n
     """
     ABOUT_TXT = """✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: {}
-✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/Moon_Knight>𝙼𝙾𝙾𝙽𝙺𝙽𝙸𝙶𝙷𝚃</a>
+✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/Mxxn_Knight>𝙼𝙾𝙾𝙽𝙺𝙽𝙸𝙶𝙷𝚃</a>
 ✯ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈: 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
 ✯ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴: 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹
 ✯ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴: 𝙼𝙾𝙽𝙶𝙾 𝙳𝙱
