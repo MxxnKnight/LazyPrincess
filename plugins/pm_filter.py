cdbdb8d706418b7307e74676ebcecb2ad1ff637a
MӨӨП KПIGΉƬ <91146761+MxxnKnight@users.noreply.github.com>
2023-04-03 22:14:49 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -580,7 +580,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         buttons = [[
              [
             InlineKeyboardButton('☆       ᴏᴡɴᴇʀ       ☆', url=f"https://telegram.me/Mxxn_Knight"),
-            InlineKeyboardButton('☆     ᴀssɪsᴛᴀɴᴛ     ☆', url='https://t.me/Mxxn_Knight_bot')
+            InlineKeyboardButton('☆     sᴇᴀʀᴄʜ     ☆', switch_inline_query_current_chat='')
             ],[
              InlineKeyboardButton('🙆🏻 Help 🦾', callback_data='help'),
             InlineKeyboardButton('♥️ About ♥️', callback_data='about')
